0974d61d89125b6d6a710b0860079afd558a9872
Brennan Batalla <brennan.batalla@gmail.com>
2020-01-03 20:40:46 -0500

    Added Procfile

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ let express = require('express'),
     router = express.Router(),
     morgan = require("morgan"),
     compression = require("compression"),
-    helmet = require("helmet");
+    helmet = require("helmet"),
+    path = require('path');
 
 app.use(morgan("common"));
 app.use(helmet());
@@ -15,15 +16,16 @@ app.use(bodyParser({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api/', require("./controllers/routes.js"));
-
 
 app.listen(port, function () {
     console.log("Server listening on port: " + port);
 });
 
-app.get('/', function (req, res, next) {
-    res.json({hello: "World"});
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 
---
+++ build/precache-manifest.49997227ced8ee5ef79a083fda4d6acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f87e6776c0e4944beed",
+    "url": "/static/css/main.8e6513bc.chunk.css"
+  },
+  {
+    "revision": "4f87e6776c0e4944beed",
+    "url": "/static/js/main.4f87e677.chunk.js"
+  },
+  {
+    "revision": "333dfe732c5c57e2a46a",
+    "url": "/static/js/1.333dfe73.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfcf711fa2c9f1c42be7baedb0372ce0",
+    "url": "/static/media/travel-map.dfcf711f.jpg"
+  },
+  {
+    "revision": "2238c64463bc7dbc6d9e88871349b0d3",
+    "url": "/index.html"
+  }
+];
